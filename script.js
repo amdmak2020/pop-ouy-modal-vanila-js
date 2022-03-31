b3fabf918f8fc5def7595e4f829e402569c2d71a
ahmed hussein <ahmedhussein0114278@gmail.com>
2022-03-31 22:44:46 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var ele = document.getElementById("clickmedaddy");
+var elex = document.getElementById("WTF");
+ele.addEventListener("click", whatitdo);
+elex.addEventListener("click", uzi);
+
+function uzi() {
+    document.getElementById('daddychill').style.display = "none"
+
+}
+
+function whatitdo() {
+
+    document.getElementById("daddychill").style.display = "grid"
+
+}
